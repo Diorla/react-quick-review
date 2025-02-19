210d4e64f56400bcc9058b80617c0ce2914ee325
Ade Adeola <adedotster@gmail.com>
2025-02-19 12:52:42 +0000

    refactor: update App component structure and remove unused sample image

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReviewForm from "./review-form";
 
 export function App() {
   return (
-    <div style={{ width: 320 }}>
+    <div className="App">
       <ReviewForm apiKey="ab92cf39-2def-4a6b-a13b-7bb79f84d885" multiple />
     </div>
   );
