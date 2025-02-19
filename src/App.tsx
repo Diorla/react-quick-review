a0864b9457fab7522f78794206414ea7e53b733a
Ade Adeola <adedotster@gmail.com>
2025-02-19 10:04:21 +0000

    feat: add logo image to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "./Button";
+const logo = new URL("./sample.png", import.meta.url).href;
 
 export function App() {
   return (
     <div>
       <h1>Hello typescript!</h1>
       <Button>Click me</Button>
+      <img src={logo} alt="logo" height={100} />
     </div>
   );
 }
